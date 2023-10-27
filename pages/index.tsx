6fc70816ec897ba1d6b4437fdbc82cc757831ec4
Md Tarvez Ansari <mdtarvezansari@192.168.1.5>
2023-10-27 21:19:45 +0530

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,70 +1,187 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import TableRow from "../components/tableRow";
+import React from 'react';
 
 const Home: NextPage = () => {
+  const details = [
+    { 'circle': 'andhra', 'customer': 'reliance', 'project': 'abc' },
+    { 'circle': 'telangana', 'customer': 'airtel', 'project': 'xyz' },
+    { 'circle': 'karnataka', 'customer': 'jio', 'project': 'lmn' },
+    { 'circle': 'tamilnadu', 'customer': 'vodafone', 'project': 'uvw' },
+    { 'circle': 'maharashtra', 'customer': 'reliance', 'project': 'rst' },
+    { 'circle': 'gujarat', 'customer': 'airtel', 'project': 'pqr' },
+    { 'circle': 'kerala', 'customer': 'jio', 'project': 'ghi' },
+    { 'circle': 'delhi', 'customer': 'vodafone', 'project': 'jkl' },
+    { 'circle': 'rajasthan', 'customer': 'reliance', 'project': 'opq' },
+    { 'circle': 'punjab', 'customer': 'airtel', 'project': 'def' },
+    { 'circle': 'haryana', 'customer': 'jio', 'project': 'abc' },
+    { 'circle': 'uttarpradesh', 'customer': 'vodafone', 'project': 'ghi' },
+    { 'circle': 'madhyapradesh', 'customer': 'reliance', 'project': 'jkl' },
+    { 'circle': 'chattisgarh', 'customer': 'airtel', 'project': 'rst' },
+    { 'circle': 'bihar', 'customer': 'jio', 'project': 'lmn' },
+    { 'circle': 'westbengal', 'customer': 'vodafone', 'project': 'opq' },
+    { 'circle': 'odisha', 'customer': 'reliance', 'project': 'uvw' },
+    { 'circle': 'assam', 'customer': 'airtel', 'project': 'def' },
+    { 'circle': 'arunachal', 'customer': 'jio', 'project': 'pqr' },
+    { 'circle': 'sikkim', 'customer': 'vodafone', 'project': 'abc' },
+    { 'circle': 'manipur', 'customer': 'reliance', 'project': 'ghi' },
+    { 'circle': 'meghalaya', 'customer': 'airtel', 'project': 'jkl' },
+    { 'circle': 'mizoram', 'customer': 'jio', 'project': 'uvw' },
+    { 'circle': 'nagaland', 'customer': 'vodafone', 'project': 'opq' },
+    { 'circle': 'tripura', 'customer': 'reliance', 'project': 'rst' },
+    { 'circle': 'jammu', 'customer': 'airtel', 'project': 'lmn' },
+    { 'circle': 'kashmir', 'customer': 'jio', 'project': 'ghi' },
+    { 'circle': 'himachal', 'customer': 'vodafone', 'project': 'abc' },
+    { 'circle': 'uttarakhand', 'customer': 'reliance', 'project': 'jkl' },
+    { 'circle': 'goa', 'customer': 'airtel', 'project': 'pqr' },
+    { 'circle': 'pondicherry', 'customer': 'jio', 'project': 'uvw' },
+    { 'circle': 'lakshadweep', 'customer': 'vodafone', 'project': 'def' },
+    { 'circle': 'andaman', 'customer': 'reliance', 'project': 'opq' },
+    { 'circle': 'nicobar', 'customer': 'airtel', 'project': 'ghi' },
+    { 'circle': 'daman', 'customer': 'jio', 'project': 'jkl' },
+    { 'circle': 'diu', 'customer': 'vodafone', 'project': 'lmn' },
+    { 'circle': 'dadra', 'customer': 'reliance', 'project': 'pqr' },
+    { 'circle': 'nagarhaveli', 'customer': 'airtel', 'project': 'uvw' },
+    { 'circle': 'chandigarh', 'customer': 'jio', 'project': 'def' },
+    { 'circle': 'delhi', 'customer': 'vodafone', 'project': 'ghi' },
+    { 'circle': 'puducherry', 'customer': 'reliance', 'project': 'jkl' },
+    { 'circle': 'chandigarh', 'customer': 'airtel', 'project': 'opq' },
+    { 'circle': 'delhi', 'customer': 'jio', 'project': 'lmn' },
+    { 'circle': 'puducherry', 'customer': 'vodafone', 'project': 'rst' },
+    { 'circle': 'chandigarh', 'customer': 'reliance', 'project': 'pqr' },
+    { 'circle': 'delhi', 'customer': 'airtel', 'project': 'ghi' },
+    { 'circle': 'puducherry', 'customer': 'jio', 'project': 'jkl' },
+    { 'circle': 'chandigarh', 'customer': 'vodafone', 'project': 'uvw' },
+    { 'circle': 'delhi', 'customer': 'reliance', 'project': 'def' },
+    { 'circle': 'puducherry', 'customer': 'airtel', 'project': 'abc' }
+  ]
+
+  const [circles, setCircles] = React.useState([]);
+  const [customer, setCustomer] = React.useState([]);
+  const [data, setData] = React.useState([
+    {
+      id: 1,
+      isChecked: false,
+      circle: "",
+      customer: "",
+      project: "",
+      date: "",
+      dnp: "",
+      tax: "",
+      tempCustomer: [],
+      tempProject: []
+    }
+  ]);
+
+  const handleAddMore = () => {
+    const tempData = [...data];
+    tempData.push({
+      id: tempData.length + 1,
+      isChecked: false,
+      circle: "",
+      customer: "",
+      project: "",
+      date: "",
+      dnp: "",
+      tax: "",
+      tempCustomer: [],
+      tempProject: []
+    })
+    setData(tempData)
+  }
+
+  const handleDelete = () => {
+    const deleteRow = data.filter(item => !item.isChecked);
+    setData(deleteRow)
+  }
+
+  const handleChecked = (id, e) => {
+    const tempData = [...data];
+    tempData.forEach(element => {
+      if (element.id === id) {
+        element.isChecked = e.target.checked
+      }
+    });
+    setData(tempData);
+  }
+
+  const handleSubmit = () => {
+    const filteredData = data.filter(item => item.circle).map(({ tempCustomer, tempProject, ...rest }) => rest);
+    console.log(filteredData);
+
+  }
+
+  const handleCirlces = (id, value) => {
+    const tempData = [...data];
+    tempData.forEach(element => {
+      if (element.id === id) {
+        element.circle = value
+        element.tempCustomer = [...new Set(details.map(item => item.circle === value && item.customer))].filter(item => item)
+      }
+    });
+    setData(tempData);
+  }
+
+  const handleCustomer = (id, value) => {
+    const tempData = [...data];
+    tempData.forEach(element => {
+      if (element.id === id) {
+        element.customer = value
+        element.tempProject = [...new Set(details.map(item => item.customer === value && item.project))].filter(item => item)
+      }
+    });
+    setData(tempData);
+  }
+
+  const handleProject = (id, value) => {
+    const tempData = [...data];
+    tempData.forEach(element => {
+      if (element.id === id) {
+        element.project = value
+      }
+    });
+    setData(tempData);
+  }
+
+  React.useEffect(() => {
+    const distinctCircles = [...new Set(details.map(item => item.circle))];
+    setCircles(distinctCircles);
+  }, [])
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div>
+        <button style={{ marginRight: "10px" }} onClick={handleSubmit}>Submit</button>
+        <button style={{ marginRight: "10px" }} onClick={handleDelete}>Delete</button>
+        <button onClick={handleAddMore}>Add More POIs</button>
+      </div>
+      <div style={{ marginTop: "10px" }}>
+        <table>
+          <thead>
+            <tr>
+              <th>Check</th>
+              <th>S.No</th>
+              <th>Circle</th>
+              <th>Customer</th>
+              <th>Project</th>
+              <th>Date</th>
+              <th>DNP</th>
+              <th>Tax Slab</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(item => <TableRow
+              data={item}
+              key={item.id}
+              circles={circles}
+              handleChecked={handleChecked}
+              handleCirlces={handleCirlces}
+              handleCustomer={handleCustomer}
+              handleProject={handleProject} />)}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
